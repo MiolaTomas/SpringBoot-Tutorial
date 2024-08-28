a64b625206de3a50a280d61f2a73722c42627f87
Tomas Miola <tomasmiola71@gmail.com>
2024-08-28 11:25:35 -0300

    query params

--- src/main/java/com/project/Tutorial/Routes.java
+++ src/main/java/com/project/Tutorial/Routes.java
@@ -2,18 +2,27 @@ package com.project.Tutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Routes {
     @GetMapping("/users")
     String myFirstRoute(){
-        return "This is my first users endpoint";
+        return "This is my users endpoint";
     }
 
+    //PathVariable
     @GetMapping("/users/{id}")
     String userById(@PathVariable int id){
         return "User id: " + id;
     }
+
+    //Query Params
+    @GetMapping("/users/{id}")
+    String queryParams(@PathVariable int id, @RequestParam String name){
+        return "User id: " + id + " - " + "Query params name: " + name;
+    }
+
     
 }
